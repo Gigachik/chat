d07e854b255a244ff52bcdff3b25f02dea0072e2
Gigachik <56297260+Gigachik@users.noreply.github.com>
2022-07-23 22:18:53 +0300

    add project

---
+++ src/WebSocket.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+
+const WebSocket = () => {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState("");
+    const [connected, setConnected] = useState(true);
+    const [username, setUsername] = useState("");
+    const socket = useRef();
+
+    function connect() {
+        socket.current = new WebSocket("ws://localhost:3000");
+
+        socket.current.onopen = () => {
+            setConnected(true);
+            const message = {
+                event: "connection",
+                username,
+                id: Date.now(),
+            };
+            console.log("Socket установлен");
+            socket.current.send(JSON.stringify(message));
+        };
+        socket.current.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            setMessages((prev) => [message, ...prev]);
+            console.log("Socket закрыт");
+        };
+        socket.current.onclose = () => {
+            console.log("Socket закрыт");
+        };
+        socket.current.onerror = () => {
+            console.log("Socket ошибка");
+        };
+    }
+
+    const sendMessage = async () => {
+        const message = {
+            username,
+            message: value,
+            id: Date.now(),
+            event: "message",
+        };
+        socket.current.send(JSON.stringify(message));
+        setValue("");
+    };
+
+    if (!connected) {
+        return (
+            <div>
+                <input
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                    placeholder="UserName"
+                />
+                <button onClick={connect}>Log In</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                type="text"
+                placeholder="UserName"
+            />
+            <button onClick={sendMessage}>Send</button>
+        </div>
+    );
+};
+
+export default WebSocket;
